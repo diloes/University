9d37da446cae83a710765bb13e4f93d055899050
diloes <diloes@hotmail.com>
2022-08-07 23:35:41 +0200

    Session 2 - Created DataBase Curso for exercise

---
+++ Backend/university/universityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string DescriptionShort { get; set; } = string.Empty;
+
+        [StringLength(280)]
+        public string DescriptionLong { get; set; } = string.Empty;
+
+        public string TargetAudience { get; set; } = string.Empty;
+
+        public string Goals { get; set; } = string.Empty;
+
+        public string Requeriments { get; set; } = string.Empty;
+
+        [Required]
+        public Levels Level { get; set; }
+    }
+
+    public enum Levels
+    {
+       Basic,
+       Intermediated,
+       Advanced
+    }
+}
--- Backend/university/universityApiBackend/obj/Debug/net6.0/universityApiBackend.AssemblyInfo.cs
+++ Backend/university/universityApiBackend/obj/Debug/net6.0/universityApiBackend.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("UniversityApiBackend")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("universityApiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("UniversityApiBackend")]
-[assembly: System.Reflection.AssemblyTitleAttribute("UniversityApiBackend")]
+[assembly: System.Reflection.AssemblyProductAttribute("universityApiBackend")]
+[assembly: System.Reflection.AssemblyTitleAttribute("universityApiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
